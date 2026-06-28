9676ed1d20fd1d159280d6de2a677510be61d9cb
hitesh <saihitesh01@gmail.com>
2026-06-28 11:09:50 +0530

    mount point

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import os
 
 app_name = os.getenv("APP_NAME", "World")
 print(f"Hello {app_name}!")
+
+print("This is a simple Python application.")
+
+print("You can set the APP_NAME environment variable to change the greeting.")
